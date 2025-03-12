0793ceceb12481f4f8a31b9652d723bdbad68e61
Lo <lorenzo.lamazzi@gmail.com>
2025-03-12 11:52:05 +0100

    add run_model_example.py

---
+++ models/run_model_example.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+
+# Location of tflite model file
+model_path = "nome_modello.tflite"
+
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path=model_path)
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Allocate tensors
+interpreter.allocate_tensors()
+
+# Get input shape
+input_shape = input_details[0]['shape']
+# Generate random input
+input_data = np.random.rand(*input_shape).astype(np.float32)
+
+# Run model
+interpreter.set_tensor(input_details[0]['index'], input_data)
+interpreter.invoke()
+
+# Get results
+output_data = interpreter.get_tensor(output_details[0]['index'])
+# Compute probabilities
+probabilities = tf.nn.softmax(output_data)
+# Compute predictions 
+predictions = np.argmax(probabilities, axis=-1)
+print("Classe predetta:", predictions)  # Classe più probabile per ogni batch
