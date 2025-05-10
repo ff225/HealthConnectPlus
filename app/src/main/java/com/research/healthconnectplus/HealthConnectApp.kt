e873c3662ad81714356d0a342b0d34f45a2e9580
ff225 <frafranco16@gmail.com>
2025-05-10 11:57:01 +0200

    add periodic work requests for heart rate and step data collection

--- app/src/main/java/com/research/healthconnectplus/HealthConnectApp.kt
+++ app/src/main/java/com/research/healthconnectplus/HealthConnectApp.kt
@@ -3,8 +3,14 @@ package com.research.healthconnectplus
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.research.healthconnectplus.data.AppRepoContainer
 import com.research.healthconnectplus.data.AppRepoContainerImpl
+import com.research.healthconnectplus.workers.HCReadHeartRate
+import com.research.healthconnectplus.workers.HCReadSteps
+import java.util.concurrent.TimeUnit
 
 class HealthConnectApp : Application() {
 
@@ -29,5 +35,34 @@ class HealthConnectApp : Application() {
 
         appRepoContainer = AppRepoContainerImpl(this)
 
+        val preferencesManager = PreferencesManager(this)
+        val workManager = WorkManager.getInstance(this)
+
+        preferencesManager.getEnabledHealthDataTypes().forEach {
+            when (it) {
+                "steps" -> {
+                    if (preferencesManager.getCollectStepData()) {
+                        workManager.enqueueUniquePeriodicWork(
+                            "HCReadSteps",
+                            ExistingPeriodicWorkPolicy.KEEP,
+                            PeriodicWorkRequestBuilder<HCReadSteps>(15, TimeUnit.MINUTES).build()
+                        )
+                    }
+                }
+
+                "heart_rate" -> {
+                    if (preferencesManager.getCollectHeartRateData()) {
+                        workManager.enqueueUniquePeriodicWork(
+                            "HCReadHeartRate",
+                            ExistingPeriodicWorkPolicy.KEEP,
+                            PeriodicWorkRequestBuilder<HCReadHeartRate>(
+                                15,
+                                TimeUnit.MINUTES
+                            ).build()
+                        )
+                    }
+                }
+            }
+        }
     }
 }
